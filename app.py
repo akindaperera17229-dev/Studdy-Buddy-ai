b44ff64370cbab220c6786e585c17b8af5701b5f
akindaperera17229-dev <akinda.perera17229@gmail.com>
2026-04-21 22:17:18 +0530

    Ready for the deployment on render platform

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import os
 from flask import Flask, request, jsonify, render_template
 from routes.ask import ask_blueprint
 
@@ -11,9 +10,9 @@ def home():
     return render_template('index.html')
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+if __name__ == '__main__':
+    app.run()
+
 
 
 
